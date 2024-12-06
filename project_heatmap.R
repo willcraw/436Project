f705ad1203f11d6f8c1806c59188062b64827f3f
willcraw <124630947+willcraw@users.noreply.github.com>
2024-12-06 17:45:00 -0600

    Update project_heatmap.R

--- project_heatmap.R
+++ project_heatmap.R
@@ -1,3 +1,4 @@
+
 library(dplyr)
 library(ggplot2)
 library(tidyverse)
@@ -13,7 +14,16 @@ to_plot %>% ggplot() + geom_tile(aes(x=year, y=Country, fill=avg_infl)) + scale_
 p = list()
 
 make_plot = function(df, given_type){
-  df %>% drop_na() %>% filter(year %in% c(2019, 2020, 2021, 2022), type == given_type, Country %in% c("India", "China", "United States", "Indonesia", "Pakistan", "Nigeria", "Brazil", "Bangladesh", "Russian Federation", "Ethiopia", "Mexico", "Japan", "Egypt, Arab Rep.", "Philippines", "Congo, Dem. Rep.", "Vietnam", "Iran, Islamic Rep.", "Turkey", "Germany", "Thailand")) %>%  ggplot() + geom_tile(aes(x=year, y=Country, fill=rate)) + scale_fill_gradient(low="white", high = "red", limits = c(-10, 100)) + labs(fill=given_type)
+  
+
+  
+filtered_data = df %>% drop_na() %>% filter(year %in% c(2019, 2020, 2021, 2022), type == given_type, Country %in% c("India", "China", "United States", "Indonesia", "Pakistan", "Nigeria", "Brazil", "Bangladesh", "Russian Federation", "Ethiopia", "Mexico", "Japan", "Egypt, Arab Rep.", "Philippines", "Congo, Dem. Rep.", "Vietnam", "Iran, Islamic Rep.", "Turkey", "Germany", "Thailand"))
+
+filtered_data <- filtered_data %>%
+  mutate(Country = factor(Country, levels = names(sort(tapply(rate, Country, mean)))))
+
+
+ggplot(filtered_data) + geom_tile(aes(x=year, y=Country, fill=rate)) + scale_fill_gradient(low="white", high = "red", limits = c(-10, 100)) + labs(fill=given_type)
 }
 
 p[["p_hcpi"]] = make_plot(df, "Headline Consumer Price Inflation")
@@ -26,8 +36,8 @@ p[["p_occpi"]] = make_plot(df, "Official Core Consumer Price Inflation")
 
 p[["p_ppi"]] = make_plot(df, "Producer Price Inflation")
 
+
+
 p[["p_avg_all_types"]] = df %>% drop_na() %>% filter(year %in% c(2019, 2020, 2021, 2022), Country %in% c("India", "China", "United States", "Indonesia", "Pakistan", "Nigeria", "Brazil", "Bangladesh", "Russian Federation", "Ethiopia", "Mexico", "Japan", "Egypt, Arab Rep.", "Philippines", "Congo, Dem. Rep.", "Vietnam", "Iran, Islamic Rep.", "Turkey", "Germany", "Thailand")) %>% group_by(Country, year) %>% summarise(avg_infl = mean(rate)) %>% ggplot() + geom_tile(aes(x=year, y=Country, fill=avg_infl)) + scale_fill_gradient(low="white", high = "red", limits = c(-10, 100)) + labs(fill="Average Inflation (all types)")
 
 p[["p_hcpi"]] + p[["p_ecpi"]] + p[["p_fcpi"]] + p[["p_occpi"]] + p[["p_ppi"]] + p[["p_avg_all_types"]]
-
-
